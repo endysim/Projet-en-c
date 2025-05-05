c2112be2f0b0029c0490043f84afb4de545188d5
Dnalor7 <166344354+Dnalor7@users.noreply.github.com>
2025-05-05 16:48:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,9 @@ typedef struct {
   int damage1;
   char attaque2[100];
   int damage2;
+  int attDelay2;
   char attaque3[100];
+  int attDelay3;
   int spe;
   int soin;
 } Stat;
@@ -26,8 +28,10 @@ void Init(Stat j[]) {
   j[0].pv = 100;
   strcpy(j[0].attaque1, "Flèche");
   j[0].damage1 = 7;
+  j[0].attDelay2 = 2;
   strcpy(j[0].attaque2, "Flèche explosive");
   j[0].damage2 = 15;
+  j[0].attDelay3 = 3;
   strcpy(j[0].attaque3, "Flèche empoisonnée");
   j[0].spe = 50;
   j[0].soin = 0;
@@ -36,8 +40,10 @@ void Init(Stat j[]) {
   j[1].pv = 150;
   strcpy(j[1].attaque1, "Coup d'épée");
   j[1].damage1 = 10;
+  j[1].attDelay2 = 2;
   strcpy(j[1].attaque2, "Coup de hache");
   j[1].damage2 = 20;
+  j[1].attDelay3 = 3;
   strcpy(j[1].attaque3, "Coup de masse");
   j[1].spe = 30;
   j[1].soin = 0;
@@ -46,8 +52,10 @@ void Init(Stat j[]) {
   j[2].pv = 80;
   strcpy(j[2].attaque1, "Boule de feu");
   j[2].damage1 = 40;
+  j[2].attDelay2 = 2;
   strcpy(j[2].attaque2, "Boule de glace");
   j[2].damage2 = 50;
+  j[2].attDelay3 = 3;
   strcpy(j[2].attaque3, "Boule de foudre");
   j[2].spe = 60;
   j[2].soin = 0;
@@ -56,8 +64,10 @@ void Init(Stat j[]) {
   j[3].pv = 100;
   strcpy(j[3].attaque1, "Soin léger");
   j[3].damage1 = 5;
+  j[3].attDelay2 = 2;
   strcpy(j[3].attaque2, "Soin modéré");
   j[3].damage2 = 10;
+  j[3].attDelay3 = 3;
   strcpy(j[3].attaque3, "Soin puissant");
   j[3].spe = 15;
   j[3].soin = 40;
@@ -66,8 +76,10 @@ void Init(Stat j[]) {
   j[4].pv = 100;
   strcpy(j[4].attaque1, "Charge rapide");
   j[4].damage1 = 15;
+  j[4].attDelay2 = 2;
   strcpy(j[4].attaque2, "Bouclier destructeur");
   j[4].damage2 = 25;
+  j[4].attDelay3 = 3;
   strcpy(j[4].attaque3, "Spin dash");
   j[4].spe = 35;
   j[4].soin = 5;
@@ -76,8 +88,10 @@ void Init(Stat j[]) {
   j[5].pv = 100;
   strcpy(j[5].attaque1, "Coup de marteau");
   j[5].damage1 = 15;
+  j[5].attDelay2 = 2;
   strcpy(j[5].attaque2, "Coup de feu");
   j[5].damage2 = 25;
+  j[5].attDelay3 = 3;
   strcpy(j[5].attaque3, "Super saut");
   j[5].spe = 35;
   j[5].soin = 10;
@@ -85,20 +99,16 @@ void Init(Stat j[]) {
 void Liste(Stat j[], int n) {
   printf("Liste des personnages :\n");
 
-  for (int i = 0; i < n+1; i+=2) {
-    printf("%d -> %s                           %d -> %s\n", i, j[i].nom, i + 1,
-           j[i + 1].nom);
-    printf("| pv: %d |                           | pv: %d |\n", j[i].pv,
-           j[i + 1].pv);
-    printf("| %s: %d |                           | %s: %d |\n", j[i].attaque1,
-           j[i].damage1, j[i + 1].attaque1, j[i + 1].damage1);
-    printf("| %s: %d |                           | %s: %d |\n", j[i].attaque2,
-           j[i].damage2, j[i + 1].attaque2, j[i + 1].damage2);
-    printf("| %s: %d |                           | %s: %d |\n", j[i].attaque3,
-           j[i].spe, j[i + 1].attaque3, j[i + 1].spe);
-    printf("| soin: %d |                           | soin: %d |\n", j[i].soin,
-           j[i + 1].soin);
-  }
+  for (int i = 0; i < n + 1; i += 2) {
+    printf("%2d -> %-12s                %15d -> %-12s\n", i, j[i].nom, i + 1, j[i + 1].nom);
+    printf("| pv:   %17d   |                | pv:   %17d   |\n", j[i].pv, j[i + 1].pv);
+    printf("| %-20s: %3d |                | %-20s: %3d |\n", j[i].attaque1, j[i].damage1, j[i + 1].attaque1, j[i + 1].damage1);
+    printf("| %-20s: %3d |                | %-20s: %3d |\n", j[i].attaque2, j[i].damage2, j[i + 1].attaque2, j[i + 1].damage2);
+    printf("| %-20s: %3d |                | %-20s: %3d |\n", j[i].attaque3, j[i].spe, j[i + 1].attaque3, j[i + 1].spe);
+    printf("| soin:   %17d |                | soin:   %17d |\n", j[i].soin, j[i + 1].soin);
+    printf("\n");
+}
+
 }
 
 void ChoixJoueur(Stat dest[], Stat source[]) {
@@ -125,34 +135,17 @@ void Affichage(Stat j1[], Stat j2[], int choix, int a, int c) {
   if (choix == 1) {
     printf("Joueur 1:                          Joueur 2:\n");
     for (int i = 0; i < 3; i++) {
-      if (a == i) {
-        p1 = "💥";
-      } else{
-        p1 = "";
-      }
-      if (c == i) {
-        p2 = "💥";
-      } else{
-        p2 = "";
-      }
-
-      if (j1[i].pv <= 0){
-        d1 = "💀";
-      }
-      else{
-        d1 = "";
-      }
-
-      if (j2[i].pv <= 0){
-        d2 = "💀";
-      }
-      else{
-        d2 = "";
-      }
-
-      printf("-%s%s (pv: %d)%s                   %50s-%s (pv: %d)%s\n", d1,j1[i].nom,
-             j1[i].pv, p1, p2, j2[i].nom, j2[i].pv,d2);
-    }
+      if (a == i) p1 = "💥"; else p1 = "  ";
+      if (c == i) p2 = "💥"; else p2 = "  ";
+      if (j1[i].pv <= 0) d1 = "💀"; else d1 = "  ";
+      if (j2[i].pv <= 0) d2 = "💀"; else d2 = "  ";
+  
+      // Nom formaté à largeur fixe (ex: 10 caractères max, ajustable)
+      printf("-%s%-10s (pv: %3d) %s    |    %s-%-10s (pv: %3d)%s\n",
+             d1, j1[i].nom, j1[i].pv, p1,
+             p2, j2[i].nom, j2[i].pv, d2);
+  }
+  
   } else if (choix == 2) {
     printf("Joueur 1:                         Bot:\n");
     for (int i = 0; i < 3; i++) {
@@ -179,30 +172,61 @@ void AttaqueJ1(Stat j1[], Stat j2[], int *joueur1, int *joueur2, int c) {
 
     printf("🔫 MENU D'ATTAQUE 🔫\n");
     printf("1 -> %s (%d dmg)\n", j1[choix].attaque1, j1[choix].damage1);
-    printf("2 -> %s (%d dmg)\n", j1[choix].attaque2, j1[choix].damage2);
-    printf("3 -> %s (%d dmg)\n", j1[choix].attaque3, j1[choix].spe);
+    printf("2 -> %s(%d) (%d dmg)\n", j1[choix].attaque2, j1[choix].attDelay2, j1[choix].damage2);
+    printf("3 -> %s(%d) (%d dmg)\n", j1[choix].attaque3, j1[choix].attDelay3, j1[choix].spe);
     printf("4 -> Soin (+%d pv)\n", j1[choix].soin);
     int entreeValide = 0;
-    while (!entreeValide) {
-    printf("Joueur1(%s) attaque joueur2(%s) avec: ", j1[choix].nom, j2[cible].nom);
+
+    do {
+        printf("joueur1 (%s) attaque joueur2 (%s) avec : ", j1[choix].nom, j2[cible].nom);
     
-    if (scanf("%d", &attaque) == 1 && attaque >= 1 && attaque <= 4) {
-        entreeValide = 1; // Saisie correcte
-    } else {
-        printf("❌ Entrée invalide. Veuillez saisir un nombre entre 1 et 4.\n");
-        while (getchar() != '\n'); // Vider le buffer
-    }
-}
+        if (scanf("%d", &attaque) != 1) {
+            printf("❌ Entrée invalide. Veuillez saisir un nombre entre 1 et 4.\n");
+            while (getchar() != '\n'); // Vider le buffer
+            continue;
+        }
+    
+        if (attaque < 1 || attaque > 4) {
+            printf("❌ Veuillez saisir un nombre entre 1 et 4.\n");
+            continue;
+        }
+    
+        if (attaque == 2 && j1[choix].attDelay2 > 0) {
+            printf("❌ Vous ne pouvez pas encore utiliser %s (rechargement en cours).\n", j1[choix].attaque2);
+            continue;
+        }
+    
+        if (attaque == 3 && j1[choix].attDelay3 > 0) {
+            printf("❌ Vous ne pouvez pas encore utiliser %s (rechargement en cours).\n", j1[choix].attaque3);
+            continue;
+        }
+    
+        // Si on arrive ici, l'entrée est valide
+        entreeValide = 1;
+    
+    } while (!entreeValide);
+    
 
     if (attaque == 1) {
         j2[cible].pv -= j1[choix].damage1;
-    } else if (attaque == 2) {
+    } else if (attaque == 2 && j1[choix].attDelay2 <= 0) {
         j2[cible].pv -= j1[choix].damage2;
-    } else if (attaque == 3) {
+        j1[choix].attDelay2 = 2;
+    } else if (attaque == 3 && j1[choix].attDelay3 <= 0) {
         j2[cible].pv -= j1[choix].spe;
+        j1[choix].attDelay3 = 3;
     } else {
         j1[choix].pv += j1[choix].soin;
         if (j1[choix].pv > j1[choix].pv) j1[choix].pv = j1[choix].pv;
+    }
+
+    for(int i = 0; i < 3; i++){
+      if(j2[i].attDelay2 > 0){
+        j2[i].attDelay2--;
+      }
+      if(j2[i].attDelay3 > 0){
+        j2[i].attDelay3--;
+      }
     }
 
     if (j2[cible].pv <= 0) {
@@ -227,22 +251,61 @@ void AttaqueJ2(Stat j1[], Stat j2[], int *joueur1, int *joueur2, int c) {
 
     printf("🔫 MENU D'ATTAQUE 🔫\n");
     printf("1 -> %s (%d dmg)\n", j2[choix].attaque1, j2[choix].damage1);
-    printf("2 -> %s (%d dmg)\n", j2[choix].attaque2, j2[choix].damage2);
-    printf("3 -> %s (%d dmg)\n", j2[choix].attaque3, j2[choix].spe);
+    printf("2 -> %s(%d) (%d dmg)\n", j2[choix].attaque2, j2[choix].attDelay2, j2[choix].damage2);
+    printf("3 -> %s(%d) (%d dmg)\n", j2[choix].attaque3, j2[choix].attDelay3, j2[choix].spe);
     printf("4 -> Soin (+%d pv)\n", j2[choix].soin);
-    printf("joueur2(%s) attaque joueur1(%s) avec: ", j2[choix].nom,
-        j1[cible].nom);
-    scanf("%d", &attaque);
+    int entreeValide = 0;
+
+do {
+    printf("joueur2 (%s) attaque joueur1 (%s) avec : ", j2[choix].nom, j1[cible].nom);
+
+    if (scanf("%d", &attaque) != 1) {
+        printf("❌ Entrée invalide. Veuillez saisir un nombre entre 1 et 4.\n");
+        while (getchar() != '\n'); // Vider le buffer
+        continue;
+    }
+
+    if (attaque < 1 || attaque > 4) {
+        printf("❌ Veuillez saisir un nombre entre 1 et 4.\n");
+        continue;
+    }
+
+    if (attaque == 2 && j2[choix].attDelay2 > 0) {
+        printf("❌ Vous ne pouvez pas encore utiliser %s (rechargement en cours).\n", j1[choix].attaque2);
+        continue;
+    }
+
+    if (attaque == 3 && j2[choix].attDelay3 > 0) {
+        printf("❌ Vous ne pouvez pas encore utiliser %s (rechargement en cours).\n", j1[choix].attaque3);
+        continue;
+    }
+
+    // Si on arrive ici, l'entrée est valide
+    entreeValide = 1;
+
+} while (!entreeValide);
+
 
     if (attaque == 1) {
         j1[cible].pv -= j2[choix].damage1;
-    } else if (attaque == 2) {
+    } else if (attaque == 2 && j2[choix].attDelay2 <= 0) {
         j1[cible].pv -= j2[choix].damage2;
-    } else if (attaque == 3) {
+        j2[choix].attDelay2 = 2;
+    } else if (attaque == 3 && j2[choix].attDelay3 <= 0) {
         j1[cible].pv -= j2[choix].spe;
+        j2[choix].attDelay3 = 3;
     } else {
         j2[choix].pv += j2[choix].soin;
         if (j2[choix].pv > j2[choix].pv) j2[choix].pv = j2[choix].pv;
+    }
+
+    for(int i = 0; i < 3; i++){
+      if(j1[i].attDelay2 > 0){
+        j1[i].attDelay2--;
+      }
+      if(j1[i].attDelay3 > 0){
+        j1[i].attDelay3--;
+      }
     }
 
     if (j1[cible].pv <= 0) {
