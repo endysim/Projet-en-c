2cf36da6502f8f862dd4a5a817b09fd4d80ff1ee
endysim <159934973+endysim@users.noreply.github.com>
2025-05-12 15:09:12 +0200

    Update Imports.c

--- Imports.c
+++ Imports.c
@@ -6,3 +6,4 @@
 #include "stb_image_write.h"
 #include <SDL2/SDL.h>
 #include <stdbool.h> // Pour utiliser le type booléen (vrai/faux)
+#include <unistd.h> //permet d’inclure l’interface POSIX qui nous permet d'utiliser sleep() 
