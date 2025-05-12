e84469c245e448e8fa0843323a35c61b2fbea999
endysim <159934973+endysim@users.noreply.github.com>
2025-05-12 15:19:31 +0200

    Update Imports.c

--- Imports.c
+++ Imports.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
-#include "stb_image_write.h" // Bibliothèque qui permet d'enregistrer une image en mémoire dans un fichier image 
+#include "stb_image_write.h" // / Inclut la bibliothèque pour écrire des images (formats PNG...)
 #include <SDL2/SDL.h>
 #include <stdbool.h> // Pour utiliser le type booléen (vrai/faux)
 #include <unistd.h> //permet d’inclure l’interface POSIX qui nous permet d'utiliser sleep() 
